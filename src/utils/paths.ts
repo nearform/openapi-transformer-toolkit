a34eeee59645aba154f0256d1536af2871d4b069
Faiz Ahmed <faizplus@users.noreply.github.com>
2024-04-24 18:45:10 +0530

    fix: oas2tson throwing error when path is provided
    when running `oas2tson -i ./example/openapi.yml -o ./example/output/ts -p paths`

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,6 +1,8 @@
 import { join as joinDesm } from 'desm'
 import { join, resolve } from 'node:path'
 import { cwd } from 'node:process'
+import _camelCase from 'lodash.camelcase'
+import _upperFirst from 'lodash.upperfirst'
 
 const workingDirectory = cwd()
 const packageRoot = joinDesm(import.meta.url, '..', '..')
@@ -13,3 +15,6 @@ export const resolveFromPackageRoot = (...pathParts: string[]) =>
 
 export const resolveFromWorkingDirectory = (...pathParts: string[]) =>
   resolvePath(workingDirectory, ...pathParts)
+
+export const formatFileName = (title: string): string =>
+  _upperFirst(_camelCase(title))
