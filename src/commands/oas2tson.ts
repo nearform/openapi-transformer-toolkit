d56e9a817c3d101304f40b3e870a55293943d2d5
Faiz Ahmed <faizplus@users.noreply.github.com>
2024-04-25 00:17:19 +0530

    fix: oas2tson throwing error when path -p option is provided (#224)
    * fix: oas2tson throwing error when path is provided
    
    when running `oas2tson -i ./example/openapi.yml -o ./example/output/ts -p paths`
    
    * fix: moved @types to dev dependencies
    
    * chore: added tests for oas2tson with --path parameter

--- src/commands/oas2tson.ts
+++ src/commands/oas2tson.ts
@@ -17,6 +17,7 @@ import type { JSONSchema4 } from 'json-schema'
 import type { Oas2Tson } from '../types/Oas2Tson'
 import type SchemasMetaData from '../types/SchemasMetaData'
 import { fromSchema } from '../utils/openapi-schema-to-json-schema-wrapper.js'
+import { formatFileName } from '../utils/paths.js'
 
 const COMPONENT_REF_REGEXP = /#\/components\/schemas\/[^"]+/g
 const outputSchemasMetaData: SchemasMetaData[] = []
@@ -27,7 +28,7 @@ export const adaptSchema = (
   filename: string
 ) => {
   delete generatedSchema.$schema
-  generatedSchema.title = name
+  generatedSchema.title = formatFileName(name)
   generatedSchema.$id = `${filename}.json`
 
   if (generatedSchema.format?.includes('date')) {
@@ -46,7 +47,9 @@ const processSchema = (
     // to just use its key, so go into the parsed schema and get the
     // actual name so the files are more easily identifiable
     const name = isArray ? value.name : key
-    const filename = _trimStart(filenamify(name, { replacement: '-' }), '-')
+    const filename = formatFileName(
+      _trimStart(filenamify(name, { replacement: '-' }), '-')
+    )
 
     adaptSchema(value, name, filename)
 
@@ -93,7 +96,7 @@ const processJSON = async (
       ? $RefParser.dereference(currentSchema.path, parserOptions)
       : $RefParser.dereference(currentSchema.path))
 
-    const fileName = path.parse(currentSchema.path).name
+    const fileName = formatFileName(path.parse(currentSchema.path).name)
     const tsSchema = `export const ${fileName} = ${JSON.stringify(
       dereferencedSchema
     )} as const`
