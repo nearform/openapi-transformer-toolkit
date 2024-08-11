698ee8246e755087b1209e055c06ef634221766e
jmjf <jamee.mikell@gmail.com>
2024-08-11 18:42:44 -0400

    fix: add output files (including FooBARBaz)

---
+++ example/output/tson/Order.ts
@@ -0,0 +1,32 @@
+export const Order = {
+  type: "object",
+  properties: {
+    id: {
+      type: "integer",
+      format: "int64",
+      minimum: -9223372036854776000,
+      maximum: 9223372036854776000,
+    },
+    petId: {
+      type: "integer",
+      format: "int64",
+      minimum: -9223372036854776000,
+      maximum: 9223372036854776000,
+    },
+    quantity: {
+      type: "integer",
+      format: "int32",
+      minimum: -2147483648,
+      maximum: 2147483647,
+    },
+    shipDate: { type: "string", format: "date-time" },
+    status: {
+      type: "string",
+      description: "Order Status",
+      enum: ["placed", "approved", "delivered"],
+    },
+    complete: { type: "boolean" },
+  },
+  title: "Order",
+  $id: "Order.json",
+} as const;
