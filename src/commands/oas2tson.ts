e1577960d6a7ed309cec42f6658e5851a02a0abd
jmjf <jamee.mikell@gmail.com>
2024-08-27 14:42:07 +0000

    fix: `-p paths` works w/o dependencies; ignore -p items not in schema; comment tsType in output

--- src/commands/oas2tson.ts
+++ src/commands/oas2tson.ts
@@ -18,11 +18,39 @@ import type { Oas2Tson } from '../types/Oas2Tson'
 import type SchemasMetaData from '../types/SchemasMetaData'
 import { fromSchema } from '../utils/openapi-schema-to-json-schema-wrapper.js'
 
-// const COMPONENT_REF_REGEXP = /#\/components\/schemas\/[^"]+/g
+// We cannot generate output for securitySchemes because securitySchemes.petstore_auth.type = 'oauth2',
+// which is not a valid JS type.
+//
+// NOTE: openapi-schema-to-json-schema supports OpenAPI 3.0, which does not have the examples (plural) key
+// needed to use components.examples. Testing shows examples works because the library weakly enforces the
+// OpenAPI 3.0 rules and specification.
 const COMPONENT_REF_REGEXP =
-  /#\/components\/(callbacks|examples|headers|links|parameters|requestBodies|responses|schemas|securitySchemes)\/[^"]+/g
+  /#\/components\/(callbacks|examples|headers|links|parameters|requestBodies|responses|schemas)\/[^"]+/g
 const INVALID_JSNAME_REGEXP = /[^A-Za-z0-9$_]/g
 const outputSchemasMetaData: SchemasMetaData[] = []
+
+// If `-p paths` and paths references headers, parameters, etc., processJSON fails because it cannot find
+// files for the dependencies in tmpdir.
+//
+// Therefore, we processSchema for all supported keywords so `-p paths` works without listing dependencies.
+// Also, components.schemas could use components.examples, so even without a -p, we must ensure files for all
+// keywords are available in tmpdir so processJSON will find the files it needs.
+const supportedKeywords = [
+  'paths',
+  'webhooks',
+  'components.callbacks',
+  'components.examples',
+  'components.headers',
+  'components.links',
+  'components.parameters',
+  'components.requestBodies',
+  'components.responses',
+  'components.schemas'
+]
+
+// In strict mode, ajv fails when compile schemas with `tsType`. This regexp lets us strip it in processJSON.
+// "Date" because it's after JSON.stringify().
+const TSTYPE_DATE_REGEXP = /(tsType: "Date",)/g
 
 export const adaptSchema = (
   generatedSchema: JSONSchema4,
@@ -51,12 +79,15 @@ const processSchema = (
   schema: JSONSchema4,
   schemasPath: string,
   definitionKeyword: string,
-  isArray: boolean
+  isArray: boolean,
+  isForOutput: boolean
 ) => {
-  Object.entries(schema).forEach(([key, value]) => {
-    // for elements in an array the name would be its index if we were
-    // to just use its key, so go into the parsed schema and get the
-    // actual name so the files are more easily identifiable
+  if (typeof schema !== 'object') return
+
+  for (const [key, value] of Object.entries(schema)) {
+    // For elements in an array, the name would be its index if we were
+    // to just use its key. So, go into the parsed schema and get the
+    // actual name so the files are easier to identify.
     const name = isArray ? value.name : key
     const filename = getTrimmedName(
       `${getNamePrefix(definitionKeyword)}_${getJSName(name)}`
@@ -76,11 +107,15 @@ const processSchema = (
     const destinationDir = path.join(schemasPath, 'tempjson')
     const destinationPath = path.join(destinationDir, `${filename}.json`)
 
-    outputSchemasMetaData.push({ dir: destinationDir, path: destinationPath })
+    // Include only keywords from the -p option (plus components.schemas) in outputSchemasMetaData
+    // so processJSON will write only keywords from -p (plus components.schemas).
+    if (isForOutput) {
+      outputSchemasMetaData.push({ dir: destinationDir, path: destinationPath })
+    }
 
     fs.ensureDirSync(destinationDir)
     fs.writeFileSync(destinationPath, schemaAsString)
-  })
+  }
 }
 
 const parserOptions: ParserOptions = {
@@ -107,13 +142,16 @@ const processJSON = async (
       : $RefParser.dereference(currentSchema.path))
 
     const fileName = path.parse(currentSchema.path).name
+
     const tsSchema = `export const ${fileName} = ${JSON.stringify(
       dereferencedSchema
     )} as const`
     const formattedSchema = await prettier.format(tsSchema, {
       parser: 'typescript'
     })
-    fs.writeFileSync(path.join(schemasPath, `${fileName}.ts`), formattedSchema)
+    // comment out `tsType` lines
+    const noTSTypeSchema = formattedSchema.replace(TSTYPE_DATE_REGEXP, '// $1')
+    fs.writeFileSync(path.join(schemasPath, `${fileName}.ts`), noTSTypeSchema)
   }
   fs.removeSync(path.join(tempdir, 'tempjson'))
 }
@@ -150,17 +188,19 @@ export const runCommand = async (
     const generatedSchema = fromSchema(parsedOpenAPIContent, {
       definitionKeywords
     })
-    // parsedOpenAPIContent is the complete OpenAPI schema (json-like object). Names have "" around them.
-    // definitionKeywords is the array of `components.schemas` plus whatever is passed for -p
-    // generatedSchema seems to be the same as parsedOpenAPIContent
-    // logger.info({ generatedSchema }, '**T1')
+
+    // parsedOpenAPIContent is the complete OpenAPI schema as a JSON-like object. Names have "" around them.
+    // definitionKeywords is the array of `components.schemas` plus keywords from -p.
+    // generatedSchema seems to be the same as parsedOpenAPIContent.
 
     const tempdir = os.tmpdir()
-    definitionKeywords.forEach(key => {
+
+    for (const key of supportedKeywords) {
       const schema = _get(generatedSchema, key)
       const isArray = Array.isArray(_get(parsedOpenAPIContent, key))
-      processSchema(schema, tempdir, key, isArray)
-    })
+      const isForOutput = definitionKeywords.includes(key)
+      processSchema(schema, tempdir, key, isArray, isForOutput)
+    }
     await processJSON(schemasPath, tempdir, excludeDereferencedIds)
   } catch (error) {
     logger.warn(error, 'Failed to convert non-object attribute, skipping')
@@ -176,8 +216,7 @@ const main = () => {
     options.input,
     options.output,
     options.properties,
-    options.excludeDereferencedIds,
-    options.logger
+    options.excludeDereferencedIds
   )
 }
 
